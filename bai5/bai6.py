5fdffa8ea4478b959b89e7f3ac8ae8ba688122da
= <=>
2023-02-27 01:42:29 +0700

    first commit

--- bai5/bai6.py
+++ bai5/bai6.py
@@ -20,3 +20,21 @@ print(id(lst1)) hien id
 new_list = a[::-1] in nguoc list
 new_list = a[0:2:1] bat dau tu khong va ket thuc o 2 nhung khong lay o 2 buoc nhay mac dinh la 1
 print(new_list) """
+# b= [-5,256] thuoc khoang nay gt da duoc tao san ngoai khoang phai tao gt
+# b = 257 int(257) toa ra int(257) xong tao bien b bien b tro den int(257)
+
+""" friends = ["Jen", "Jack", "Kenny", "Jelly", "Bob", "Henry", "Anne"]
+print(friends[0:4])
+print(friends[3:])
+print(friends[0:])
+print(friends[::-1])
+new_friends = friends.copy()
+print(new_friends)
+print(friends[1:]) """
+
+""" students = [["SV001", "Bob", "23"], [
+    "SV002", "Kenny", "34"], ["SV003", "Henry", "45"]]
+print(f"ID:{students[0][0]}, name:{students[0][1]}, age:{students[0][2]}")
+print(f"tuoi SV002:{students[1][2]}")
+print(students[1:])
+print(f"Id:{students[2][0]}") """
