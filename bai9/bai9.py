90945cb89513fa815b15f60564e30c4b01070be4
= <=>
2023-02-28 00:01:11 +0700

    commit

---
+++ bai9/bai9.py
@@ -0,0 +1,35 @@
+""" 
+for
+while 
+break
+continue
+ """
+# for
+""" for _ in range(5): # list(range(0,5,1)) 0 1 2 3 4 step 1 hd vs int vd range(-4) ra []
+    print("hi") """
+""" for i in range(1, 21):
+    if i % 2 == 0:
+        print(i, end =' ')
+ """
+
+# while
+
+""" s = input('> ')
+
+while s != "q":
+    print("Hi")
+    s = input('> ') """
+
+""" for i in range(5):
+    for j in range(3):
+        print(i, j, sep=" - ") """
+
+# break thoat ra khoi vong lap chua no
+# continue bo qua cau lenh ben duoi chuyen sang vong lap moi
+ # su dong tab or space chi 1 trong 2
+for i in range(1, 21):
+    # if (i % 2 == 0):
+    #     continue
+    print(i, end=" ")
+else:
+    print("hi")    # in ra het xong thuc hien else
